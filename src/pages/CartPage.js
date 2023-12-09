9c2c21cd56d5181a49c44f00c965d82260c324be
Navya Jain <97445957+jnavya23@users.noreply.github.com>
2023-12-09 01:42:04 +0000

    Update Hero.js and CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,64 +1,85 @@
-import React from 'react';
-import { useCartContext } from '../context/cart_context';
+import React from "react";
+import { useCartContext } from "../context/cart_context";
 import styled from "styled-components";
 import CartItem from "../components/CartItem";
-import {MdClear} from "react-icons/md";
+import { MdClear } from "react-icons/md";
 
 const CartPage = () => {
-  const {cart: cartItems, total_items, total_amount, clearCart} = useCartContext();
+  const {
+    cart: cartItems,
+    total_items,
+    total_amount,
+    clearCart,
+  } = useCartContext();
 
-  if(cartItems.length < 1){
+  if (cartItems.length < 1) {
     return (
       <NotFoundWrapper>
-        <div className='container'>No items found in the cart.</div>
+        <div className="container">No items found in the cart.</div>
       </NotFoundWrapper>
-    )
+    );
   }
 
   return (
     <CartWrapper>
-      <div className='container'>
-        <div className='cart-pg-title'>
+      <div className="container">
+        <div className="cart-pg-title">
           <h3>Shopping Cart</h3>
         </div>
-        <div className='cart-grid grid'>
+        <div className="cart-grid grid">
           {/* card grid left */}
-          <div className='cart-grid-left'>
-            <div className='flex flex-wrap flex-between'>
-              <div className='cart-count-info'>
-                <span className='fw-7 fs-18'>{total_items}</span> Course in Cart
+          <div className="cart-grid-left">
+            <div className="flex flex-wrap flex-between">
+              <div className="cart-count-info">
+                <span className="fw-7 fs-18">{total_items}</span> Course in Cart
               </div>
-              <button type = "button" className='cart-clear-btn flex fs-15 fw-6 text' onClick={() => clearCart()}>
-                <MdClear className='text-pink' />
-                <span className='d-inline-block text-pink'>Clear All</span>
+              <button
+                type="button"
+                className="cart-clear-btn flex fs-15 fw-6 text"
+                onClick={() => clearCart()}
+              >
+                <MdClear className="text-pink" />
+                <span className="d-inline-block text-pink">Clear All</span>
               </button>
             </div>
 
-            <div className='cart-items-list grid'>
-              {
-                cartItems.map(cartItem => {
-                  return (
-                    <CartItem key = {cartItem.courseID} cartItem = {cartItem} />
-                  )
-                })
-              }
+            <div className="cart-items-list grid">
+              {cartItems.map((cartItem) => {
+                return <CartItem key={cartItem.courseID} cartItem={cartItem} />;
+              })}
             </div>
           </div>
           {/* end of grid left */}
           {/* cart grid right */}
-          <div className='cart-grid-right'>
-            <div className='cart-total'>
-              <span className='d-block fs-18 fw-6'>Total:</span>
-              <div className='cart-total-value fw-8'>Rs{total_amount.toFixed(2)}</div>
-              <button type = "button" className='checkout-btn bg-purple text-white fw-6'>Checkout</button>
+          <div className="cart-grid-right">
+            <div className="cart-total">
+              <span className="d-block fs-18 fw-6">Total:</span>
+              <div className="cart-total-value fw-8">
+                Rs{total_amount.toFixed(2)}
+              </div>
+              <button
+                type="button"
+                className="checkout-btn bg-purple text-white fw-6"
+              >
+                Checkout
+              </button>
+              <br />
+              <a href="https://rm38m6-3000.csb.app/">
+                <button
+                  type="button"
+                  className="checkout-btn bg-purple text-white fw-6"
+                >
+                  Add More
+                </button>
+              </a>
             </div>
           </div>
           {/* end of cart grid right */}
         </div>
       </div>
     </CartWrapper>
-  )
-}
+  );
+};
 
 const NotFoundWrapper = styled.div`
   padding: 30px 0;
@@ -67,47 +88,47 @@ const NotFoundWrapper = styled.div`
 
 const CartWrapper = styled.div`
   padding: 30px 0;
-  .card-pg-title{
+  .card-pg-title {
     padding: 20px 0 6px 0;
   }
-  .cart-grid{
+  .cart-grid {
     row-gap: 40px;
-    .cart-grid-left{
+    .cart-grid-left {
       margin-bottom: 30px;
     }
 
-    .cart-clear-btn{
-      span{
+    .cart-clear-btn {
+      span {
         margin-left: 6px;
       }
     }
 
-    .cart-items-list{
+    .cart-items-list {
       margin-top: 20px;
       row-gap: 12px;
     }
-    .cart-total-value{
+    .cart-total-value {
       font-size: 34px;
     }
-    .checkout-btn{
+    .checkout-btn {
       padding: 14px 28px;
       letter-spacing: 1px;
       margin-top: 12px;
       transition: var(--transition);
 
-      &:hover{
+      &:hover {
         background-color: var(--clr-dark);
       }
     }
-    .cart-total{
+    .cart-total {
       padding-bottom: 50px;
     }
 
-    @media screen and (min-width: 992px){
-      grid-template-columns: 70% 30%;;
+    @media screen and (min-width: 992px) {
+      grid-template-columns: 70% 30%;
       column-gap: 32px;
     }
   }
 `;
 
-export default CartPage
+export default CartPage;
